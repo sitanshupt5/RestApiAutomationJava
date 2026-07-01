7bc6ece2d1a2a7290f91509b0525abceb93f3a38
sitanshupt5 <sitanshupt5@gmail.com>
2026-07-02 01:22:59 +0530

    1. Updated FilePaths.java

--- commons/src/main/java/org/automation/api/rest/commons/constants/FilePaths.java
+++ commons/src/main/java/org/automation/api/rest/commons/constants/FilePaths.java
@@ -7,5 +7,5 @@ public class FilePaths {
     public static final String API_PATH_REQUEST_JSON =
             "/apischema/env_type/api_path/request.json";
     public static final String TEST_DATA_FILE_PATH = "/apischema/env_type/api_path/testdata.yml";
-    public static final String SCHEMA_MAPPING = "/apischema/env_type/api_path/request_response_mapping.yml";
+    public static final String SCHEMA_MAPPING = "/apischema/env_type/api_path/validation_mapping.yml";
 }
